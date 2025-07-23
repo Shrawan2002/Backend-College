a26d4a9271493b12a74e05664cd149720afb0019
Shrawan2002 <shrawankumar1754@gmail.com>
2025-07-23 20:52:06 +0530

    initial files

---
+++ EJSdir/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+ const app = express();
+
+ const port = 8000;
+
+ app.set("view engine", "ejs");
+ //jab roote directory se run hota hai to ham set kar dete hai ki views directory ko 
+ app.set("views", path.join(__dirname,"/views"));
+// express by default views directory ke andar ja ke hi check karta hai file ko
+ app.get("/",(req,res)=>{
+    res.render("home.ejs");
+ })
+
+ app.listen(port,()=>{
+    console.log(`listening on port ${port}`);
+ })
+
