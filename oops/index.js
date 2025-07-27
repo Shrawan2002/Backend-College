0f4ff399a0b2d567886e8276cd2be5a8c46fd128
Shrawan2002 <shrawankumar1754@gmail.com>
2025-07-27 12:04:48 +0530

    initial files

--- oops/index.js
+++ oops/index.js
@@ -1,17 +1,34 @@
 // factory function
 
-function personMaker(name,age){
-    const person = {
-        name:name,
-        age:age,
-        talk(){
-         console.log(`Hi, my name is ${this.name}`);
-        }
-    }
-    return person;
-}
+// function personMaker(name,age){
+//     const person = {
+//         name:name,
+//         age:age,
+//         talk(){
+//          console.log(`Hi, my name is ${this.name}`);
+//         }
+//     }
+//     return person;
+// }
 
-let p1 = personMaker("adam",25);
-let p2 = personMaker("antam",23);
+// let p1 = personMaker("adam",25);
+// let p2 = personMaker("antam",23);
 // p1.talk === p2.talk -> false
 
+// Constructor - doesn't return anything & start with capital
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+    console.log(this);
+}
+
+Person.prototype.talk = function(){
+    console.log(`Hi my name is ${this.name}`);
+}
+
+// let p1 = Person("adam",25);
+
+let p1 = new Person("adam", 25);
+let p2 = new Person("eve",23);
+
+//p1.talk === p2.talk-> true
