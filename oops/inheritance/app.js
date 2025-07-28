820962664cc31a0d2c993c22bdafbd8ff55a6dae
Shrawan2002 <shrawankumar1754@gmail.com>
2025-07-28 08:50:29 +0530

    update index.js

--- oops/inheritance/app.js
+++ oops/inheritance/app.js
@@ -16,3 +16,50 @@ class Student extends Person{
         this.mark = mark;
     }
 }
+
+class Teacher extends Person{
+    constructor(name,age,subject){
+        console.log("teacher constructor called");
+        super(name,age);
+        this.subject = subject;
+    }
+}
+
+s1 = new Student("flip",26);
+
+class Mammal{
+    constructor(name){
+        this.name = name;
+        this.type = "warm-blooded";
+    }
+
+    eat(){
+        console.log("i am eating");
+    }
+   
+}
+
+class Dog extends Mammal{
+    constructor(name){
+        super(name);
+    }
+    eat(){
+        console.log("dog is eatig");
+        // override kar dega function ko
+    }
+     bark(){
+        console.log("wooff..");
+    }
+}
+
+class Cat extends Mammal{
+    constructor(name){
+        super(name);
+    }
+   
+     meow(){
+        console.log("meow...");
+    }
+}
+
+let dog1 = new Dog("tuffie");
