a3241f807b00264ff6983c793efc252d5bdd1f66
Shrawan2002 <shrawankumar1754@gmail.com>
2025-07-30 12:58:21 +0530

    add new files

--- REST_CLASS/index.js
+++ REST_CLASS/index.js
@@ -8,6 +8,27 @@ app.set("views",path.join(__dirname,"/views"));
 
 app.use(express.static(path.join(__dirname,"public")));
 
+app.use(express.urlencoded({extended:true}));
+
+let posts = [
+    {
+        username: "apnacollege",
+        content : "I love coding"
+    },
+    {
+        username: "shrawankumar",
+        content: "Hard work is important to achieve success"
+    },
+      {
+        username: "rahulkumar",
+        content : "I got selected for my 1st internship!"
+    },
+]
+//index Route
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
+})
+
 app.listen(port,()=>{
     console.log("listening to por:",port);
 })
