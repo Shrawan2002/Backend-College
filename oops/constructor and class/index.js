8ea8599923a4bb265c2557ebe26940716e4fee1f
Shrawan2002 <shrawankumar1754@gmail.com>
2025-07-30 12:07:55 +0530

    initial files

--- oops/constructor and class/index.js
+++ oops/constructor and class/index.js
@@ -50,3 +50,14 @@ let p1 = new Person("adam", 25);
 let p2 = new Person("eve", 23);
 
 // p1.talk === p2.talk-> true
+
+let arr1 = [1,2,3];
+let arr2 = [1,2,3];
+
+arr1.sayHello = ()=>{
+    console.log("hello! i am array");
+}
+
+arr2.sayHello = ()=>{
+    console.log("hello! i am array");
+}
