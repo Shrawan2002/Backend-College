9463f82aa086705dcb1ab31b25dcc973bbfd3596
Shrawan2002 <shrawankumar1754@gmail.com>
2025-07-30 12:21:43 +0530

    update index.js

---
+++ REST_CLASS/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"/views"));
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.listen(port,()=>{
+    console.log("listening to por:",port);
+})
