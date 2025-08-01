7d96a15607805597b510fc209fc717cd8f92d3f5
Shrawan2002 <shrawankumar1754@gmail.com>
2025-08-01 11:38:44 +0530

    connect database

--- SQLCLASS/index.js
+++ SQLCLASS/index.js
@@ -1,15 +1,47 @@
 const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
 
-let  createRandomUser = ()=> {
-  return {
-    userId: faker.string.uuid(),
-    username: faker.internet.username(), // before version 9.1.0, use userName()
+
+
+// let  createRandomUser = ()=> {
+//   return {
+//     userId: faker.string.uuid(),
+//     username: faker.internet.username(), // before version 9.1.0, use userName()
+//     email: faker.internet.email(),
+//     avatar: faker.image.avatar(),
+//     password: faker.internet.password(),
+//     birthdate: faker.date.birthdate(),
+//     registeredAt: faker.date.past(),
+//   };
+// }
+
+// console.log(createRandomUser());
+
+// Create the connection to database
+const connection =  mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'delta_app',
+  password: "Shrawan@123"
+});
+
+ try{
+    connection.query("SHOW TABLES",(err,result)=>{
+    if(err) throw err;
+    console.log(result);
+})
+ }catch(err){
+    console.log(err);
+ }
+
+  let getRandomUser = ()=> {
+   return {
+    Id: faker.string.uuid(),
+    username: faker.internet.userName(), // before version 9.1.0, use userName()
     email: faker.internet.email(),
-    avatar: faker.image.avatar(),
     password: faker.internet.password(),
-    birthdate: faker.date.birthdate(),
-    registeredAt: faker.date.past(),
+    
   };
 }
 
-console.log(createRandomUser());
+console.log(getRandomUser());
