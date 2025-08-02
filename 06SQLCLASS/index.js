2fd08d8f63f868725ff6f5a6f8dcf937bf8fe349
Shrawan2002 <shrawankumar1754@gmail.com>
2025-08-02 10:01:11 +0530

    updates files

--- 06SQLCLASS/index.js
+++ 06SQLCLASS/index.js
@@ -1,6 +1,7 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
-
+const express = require("express");
+const app = express();
 
 
 // let  createRandomUser = ()=> {
@@ -26,8 +27,9 @@ const connection =  mysql.createConnection({
 });
 
 // let q = "SHOW TABLES";
+
  // Using placeholders
-let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+// let q = "INSERT INTO user (id, username, email, password) VALUES ?";
 
 // let users = [["123b", "123_newuserb", "abc@gmail.comb", "abcb"],
 //             ["123c", "123_newuserc", "abc@gmail.comc", "abcc"]];
@@ -56,14 +58,52 @@ let q = "INSERT INTO user (id, username, email, password) VALUES ?";
 // }
 
 
+//   let getRandomUser = ()=> {
+//    return [
+//      faker.string.uuid(),
+//      faker.internet.username(), 
+//      faker.internet.email(),
+//      faker.internet.password(),
+//   ];
+// }
 
-  let getRandomUser = ()=> {
-   return [
-     faker.string.uuid(),
-     faker.internet.userName(), // before version 9.1.0, use userName()
-     faker.internet.email(),
-     faker.internet.password(),
-  ];
-}
+//  let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+//  let data = [];
+
+//   for(let i=1; i<=100; i++){
+//     data.push(getRandomUser());
+//   }
+
+//   try{
+//     connection.query(q, [data], (err,result)=> {
+//       if(err) throw err;
+//       console.log(result);
+//     })
+//   }catch(err){
+//     console.log(err);
+//   }
+
+//   connection.end();
+
+  app.get("/",(rq,res)=>{
+    let q = `SELECT COUNT(*) FROM user`;
+    try{
+      connection.query(q, (err,result)=>{
+        if(err) throw err;
+          console.log(result[0]["COUNT(*)"]);
+          res.send(result[0]["COUNT(*)"]);
+      })
+    }catch(err){
+      console.log(err);
+      res.send("some err in DB");
+    }
+  
+  })
+
+  app.listen(8080,()=>{
+    console.log("server is listenning to port 8080");
+  })
+
+
 
 // console.log(getRandomUser());
