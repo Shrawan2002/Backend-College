bcac708a8ea73031b404c1dd1db5514cbde113d3
Shrawan2002 <shrawankumar1754@gmail.com>
2025-08-05 20:12:44 +0530

    mongoose

--- 03Backend/index.js
+++ 03Backend/index.js
@@ -1,25 +1,25 @@
-// const express = require("express");
-// const app = express();
-// const port = 8080;
-// //express read kar payega body ke data ko-> use middle ware
-// app.use(express.urlencoded({extended:true}));
-// //JSON data ko read karneke liye
-// app.use(express.json());
+const express = require("express");
+const app = express();
+const port = 8080;
+//express read kar payega body ke data ko-> use middle ware
+app.use(express.urlencoded({extended:true}));
+//JSON data ko read karneke liye
+app.use(express.json());
 
-// app.get("/register",(req,res)=>{
-//     // res.send("standard Get response");
-//     let {user, password} = req.query;
-//     res.send(`standard GET response. Welcome ${user}!`);
-// })
+app.get("/register",(req,res)=>{
+    // res.send("standard Get response");
+    let {user, password} = req.query;
+    res.send(`standard GET response. Welcome ${user}!`);
+})
 
-// app.post("/register",(req,res)=>{
-//     // console.log(req.body);
-//     let {user,password} = req.body;
-//     res.send(`standard POST response. Welcome ${user}!`);
-// })
+app.post("/register",(req,res)=>{
+    // console.log(req.body);
+    let {user,password} = req.body;
+    res.send(`standard POST response. Welcome ${user}!`);
+})
 
-// // app.listen(port,()=>{
-// //     console.log(`listening on port : ${port}`);
-// // })
+app.listen(port,()=>{
+    console.log(`listening on port : ${port}`);
+})
 
 
