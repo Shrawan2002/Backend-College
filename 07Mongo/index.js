0db0f390bf8ef73a9955d09a45665a5d774c572e
Shrawan2002 <shrawankumar1754@gmail.com>
2025-08-06 08:10:37 +0530

    connect to db

--- 07Mongo/index.js
+++ 07Mongo/index.js
@@ -20,3 +20,26 @@ const userSchema = new mongoose.Schema({
     email: String,
     age: Number
 })
+
+const User = mongoose.model("User",userSchema);
+
+// const user1 = new User({
+//     name: "Adam",
+//     email: "adam@yahoo.in",
+//     age: 48,
+// }); // CLI -> node -> .load index.js
+
+const user2 = new User({
+    name: "Eve",
+    email: "eve@yahoo.in",
+    age: 48
+})
+
+// user1.save();
+
+user2.save()
+.then((res)=>{
+    console.log(res);
+}).catch((err)=>{
+    console.log(err);
+})
