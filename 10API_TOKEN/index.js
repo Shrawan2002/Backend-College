3289494456e2185bfb4d70f5608322fbc612ee16
Shrawan2002 <shrawankumar1754@gmail.com>
2025-08-17 20:42:42 +0530

    custom error handling

---
+++ 10API_TOKEN/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+
+// app.use("/api",(req,res,next)=>{
+//     let {token} = req.query;
+//     if(token === "giveaccess"){
+//         next();
+//     }else{
+//         res.send("ACCESS DENIED!");
+//     }
+// })
+
+// app.get("/api", (req,res)=>{
+//     res.send("data");
+// })
+
+const checkToken = (req,res,next)=>{
+    const {token} = req.query;
+      if(token === "giveaccess"){
+        next();
+    }else{
+        //Handing Errors
+        // res.send("ACCESS DENIED!");
+        throw new Error("ACCESS DENIED!");
+    } 
+}
+
+app.get("/api", checkToken,(req,res)=>{
+    res.send("data");
+})
+
+
+// ERROR HANDLING MIDDLEWARE
+
+app.get("/err",(req,res)=>{
+    abcd = abcd;
+})
+
+app.use((err,req,res,next)=>{
+    console.log("----ERROR-----");
+    next(err)  //express ke defaulr error handling usko tigger kar rahe hai or custom error handler ko 
+})
+
+app.use((err,req,res,next)=>{
+    console.log("----ERROR2-----");
+    next(err) 
+})
+
+app.listen(8080,()=>{
+    console.log("server is run on port 8080");
+})
