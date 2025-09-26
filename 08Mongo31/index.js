bcb1319a32401ddd0ebae42058cc68117f5ae1a6
Shrawan2002 <shrawankumar1754@gmail.com>
2025-09-26 17:20:20 +0530

    add create route, date parsing

--- 08Mongo31/index.js
+++ 08Mongo31/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = require("./config/db.js");
 const Chat = require("./models/chat.js");
 const path = require("path");
+const methodOverride = require("method-override");
 const app = express();
 
 connectDB();
@@ -10,6 +11,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 app.get("/", (req,res)=>{
     res.send("i am root");
@@ -26,13 +28,23 @@ app.get("/", (req,res)=>{
     res.render("new.ejs")
  })
 
- // post 
+ // create route
  app.post("/chats", async(req,res)=>{
-    let {from, msg, to} = req.body;
-    let chat1 = new Chat({from,msg, to});
+    let {from, msg, to,  created_at} = req.body;
+    let createdAtDate = created_at ? new Date(created_at) : new Date();
+    let chat1 = new Chat({from,msg, to, created_at: createdAtDate});
     let result = await chat1.save()
     console.log(result);
     res.redirect("/chats")
+   console.log(createdAtDate);
+ })
+
+ //edit route
+
+ app.get("/chats/:id/edit",async(req,res)=>{
+   let id = req.params.id;
+   const chat = await Chat.findById(id);
+   res.render("edit.ejs", {chat});
  })
 
 
